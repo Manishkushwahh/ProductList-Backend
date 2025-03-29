2d124c744ef062e6501ddf258e113d89cefa97e4
Manishkushwahh <manishkushwah169@gmail.com>
2025-03-29 14:39:43 +0530

    Backend File Added

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ const finalproductRouter = require("./routes/finalproductRoute");
 
 app.use(cors({
     // origin: "http://localhost:5173/",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-"optionsSuccessStatus": 200
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200
     
 }));
 app.use(express.json());
